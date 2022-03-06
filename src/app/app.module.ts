d3f043dd7f241cd731e65cf2b98e1428f82fa552
CesarCejaC <tonycejacortes@gmail.com>
2022-03-05 21:51:10 -0600

    Create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/homePage/home-page.component';
+import { CommunityPageComponent } from './pages/communityPage/community-page.component';
+import { ReviewPageComponent } from './pages/reviewPage/review-page.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginPageComponent } from './routes/login/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    CommunityPageComponent,
+    ReviewPageComponent,
+    LayoutComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule
